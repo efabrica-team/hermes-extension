1f3181a434ad4b5278bef07a0ec39bea7f17df3a
Michal Lulco <lulco@efabrica.sk>
2022-10-06 23:52:21 +0200

    Added traits to dummy driver

--- src/Driver/DummyDriver.php
+++ src/Driver/DummyDriver.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace Efabrica\HermesExtension\Driver;
 
 use Closure;
+use Efabrica\HermesExtension\Heartbeat\HeartbeatBehavior;
 use Tomaj\Hermes\Dispatcher;
 use Tomaj\Hermes\Driver\DriverInterface;
+use Tomaj\Hermes\Driver\ShutdownTrait;
 use Tomaj\Hermes\MessageInterface;
 
 final class DummyDriver implements DriverInterface
 {
+    use ShutdownTrait;
+    use HeartbeatBehavior;
+
     public function send(MessageInterface $message, int $priority = Dispatcher::DEFAULT_PRIORITY): bool
     {
         return true;
